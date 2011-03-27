40c787e26919fde568ce83d67570e1fcae3a54e9
Dmytro Vorona <alendit@gmail.com>
2011-03-27 20:20:56 +0200

    Corrected deployment settings

--- src/exedjango/deployment_settings.py
+++ src/exedjango/deployment_settings.py
@@ -1,6 +1,17 @@
 # Django settings for exedjango project.
 
-import settings
+from settings import *
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+        'default' : {
+                'ENGINE' : 'django.db.backends.mysql',
+                'NAME' : 'exedjangodb',
+                'USER' : 'root',
+                'PASSWORD' : 'ssl20qwerty',
+                'HOST' : 'localhost',
+                'PORT' : '',
+        }
+}
